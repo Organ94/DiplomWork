9c83ab9a03136a5a28b5075499959fba2b83a9e3
Organ94 <99993473+Organ94@users.noreply.github.com>
2023-04-10 23:14:33 +0300

    Initial commit

---
+++ src/main/java/com/example/diplomwork/DiplomWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.diplomwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomWorkApplication.class, args);
+    }
+
+}
